5cda3b2f1833b810c4c36e116b1a9e138b180e98
KARAN-SHARXA <karansharma2302578@gmail.com>
2025-10-30 23:45:31 +0530

    passing props , function

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,6 +5,7 @@ import Nickname from './assets/componenet/Nickname'
 import Name from './assets/componenet/Name'
 import Map from './assets/componenet/Map'
 import Hook from './assets/componenet/Hook'
+import Card from './assets/componenet/Card'
 
 function App() {
   return (
@@ -14,6 +15,16 @@ function App() {
       <Nickname name="kanu" ></Nickname>
       <Map></Map>
       <Hook></Hook>
+      <Card>
+        <h1>best web dev</h1>
+        <p>trying to gon</p>
+        <p>will complete the course</p>
+      </Card>
+
+      <Card>
+        hello ji kase ho srae
+      </Card>
+      
     </div>
   )
 }
